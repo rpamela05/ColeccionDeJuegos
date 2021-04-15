3838bcaa7a2057555211f213ce52fc2c6cc7837d
Ruth Pamela <ruth.carbajal@tecsup.edu.pe>
2021-04-15 12:40:48 -0700

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Ruth Carbajal on 4/15/21.
+//  Copyright © 2021 Ruth Carbajal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
